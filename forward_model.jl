360e8132205e05e0980234aa71aed587243aa8dd
Ben Bougher <ben.bougher@gmail.com>
2015-03-22 13:37:29 -0700

    small corrections

--- forward_model.jl
+++ forward_model.jl
@@ -2,22 +2,24 @@
     Wavefield Modelling in 10 lines
 
 The physics of seismic imaging is modelled by the elastic wave equation, which
-in practice is reduced to a crude acoustic approximation. The cavelier
+in practice is reduced to an acoustic approximation. The cavelier
 assumption that waves propagate through a heterogenous mechanical soup as
 ripples in a pond has provided the physical footing for wavefield modelling, the
 workhorse behind modern migration and inversion techniques.
 
 This essay solves the acoustic wave-equation for a single frequency point
-source, which is equivalent to solving a Helmholtz system.
-Although this may sound like a trivial exercise, I assure you I am not
-wasting your time. Recall 10th grade physics class (gr 6 if you were
-educated outside of Canada) and the principle of superposition. Any waveform
-can be built by a superposition of single frequencies, and any wavefield can be
-represented by a superposition of point sources. Awesome! Add a for loop and
-you can now model any physical wavefield.
+source, which is equivalent to solving the Helmholtz system:
+ \nabla^2U + \omega^2mU = q(x)
+where U is the wavefield, m is the slowness squared, \omega is the frequency,
+and q is the source term
+
+Although this example may sound trivial, superposition tells us that any waveform
+can be built by a synthesis of single frequencies, and any wavefield can be
+represented by a superposition of point sources. With a for loop
+you can model any physical wavefield from this basic code snippet.
 
 For the sake of simplicity, build a constant velocity model on a small 2-D
-grid. To avoid floating point rounding errors, I will use km.
+grid. To avoid floating point rounding errors, I will use km as units.
 =#
     # make a 3 km by 3 km  constant velocity model
     n, dn = 40, .01 # 10 m spacing
@@ -26,10 +28,9 @@ grid. To avoid floating point rounding errors, I will use km.
 
 #=
 Now I need to make a discrete La Place operator. This will look a computational
-voodoo, but keeping things in matrix form simplifies the problem. Start with
-the 1-D discrete La Place equation, and notice that this expression can be
-written as a matrix-vector product. Using kronecker products, this is expanded
-into a 2D Laplacian operator.
+voodoo, but keeping things in matrix form simplifies the problem. The discrete
+LaPlacian operator can be represented as band diagonal matrix(ref), which we
+extend to 2-D using a kronecker product(footnote).
 =#    
     # Make the LaPlacian operator
     Dx = spdiagm(tuple(ones(n-1), -ones(n)*2, ones(n-1) ), [1,0,-1])
@@ -55,7 +56,8 @@ Since matrices operate on vectors, we need to flatten our models into vectors.
     q = q[:]
 
 #=
-We can now solve the Helmholtz equation and see our modelled wavefield.
+We can now solve the Helmholtz equation and see our modelled wavefield. Note
+that solving this discrete PDE is solving a system a of linear equations. 
 =#
     
     # Make the Helmholtz operator
@@ -64,11 +66,12 @@ We can now solve the Helmholtz equation and see our modelled wavefield.
     # Hu = q solve for the wavefield
     u = H \q
 
+    # reshape the factor back to our model dimensions
+    U = reshape(u,n,n)
+
 using PyCall
 @pyimport matplotlib.pyplot as plt
 
-    # reshape the wavefield vector
-    u = reshape(u, n,n)
 
-    plt.imshow(u)
+    plt.imshow(U)
     plt.show()
