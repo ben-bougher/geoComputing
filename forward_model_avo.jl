1b26c07b379183d51e851792be274db8a3adc5fe
ben-bougher <ben.bougher@gmail.com>
2015-06-07 21:16:41 -0700

    working forward avo model Am=d

--- forward_model_avo.jl
+++ forward_model_avo.jl
@@ -2,64 +2,86 @@
 Am=d
 
 In 52 things about geophysics, Bryan Russel wrote an excellent article
-on Am=d. This simple equation is at the core of most seismic 
+on Am=d, the simple matrix-vector product at the core of most seismic 
 modelling and processing. This article demonstrates this relationship 
-by forward modelling a seismic experiment using the linear Am=d.
+by forward modelling a seismic experiment using the linear model Am=d.
 
-First and for most, I will put some clear definitions on what these
+First and for most, let's put clear definitions on what these
 algebraic symbols represent. The "vector" m is the model of the earth,
-which acts as input parameters of the experiment. It is very counter-intuitive 
-to think of earth model as 1D vector instead of a volume. It helps to think of
-the earth model as a list of inputs into our equation, not as a geometrical
-representation of the earth. The matrix A describes the 
-mechanics of our forward model, and transforms our vector of earth parameters m
-into a vector of observed data d. Generally, m is the physical properties of the
-earth and A is the physics that models the data.
+which acts as the input parameters of the experiment. If it seems counter-intuitive 
+to represent the earth as a 1D vector instead of a slice or volume, think of
+the earth model as a list of inputs into our experiment, not as a geospatial
+representation. The matrix A describes the mechanics of our 
+forward model, and transforms our vector of earth parameters m
+into a vector of observed data d. Generally, m is the physical 
+properties of the earth and A is the physics the models data.
 
 This article demonstrates a simple forward model of an angle gather typically 
-used in AVO analysis. In this case m will the elastic properties of the earth,
+used in AVO analysis. In this case m is the elastic properties of the earth,
 which for simplicity we will define in the time domain.
 
-=#
-using SpecialMatrices
-using PyCall
-include("helper.jl")
-    
-@pyimport matplotlib.pyplot as plt
-    
-#=
-Build a 1D model with vp, vs, and rho.
-=#  
-n = 1001
-vp, vs, rho = zeros(n), zeros(n), zeros(n)
-vp[0:300] = 1500; vs[0:300] = 0; rho[0:300] = 1000.0;
-vp[300:750] = 3500; vs[300:750] = 2000; rho[300:750] = 3000.0;
-vp[750:end] = 5000; vs[750:end] = 2500; rho[750:end] = 3500.0;
-
-# Add 10% some noise to the earth model
-vp += randn(n)*.1*mean(vp)
-vs += randn(n)*.1*mean(vs)
-rho += randn(n)*.1*mean(rho)
-
-#=
 Using the aki-richards equation, the angle-dependent p-wave reflectivity
 can be modelled by the linear equation:
     aki richards
-Defining a gradient operator and lumping the constant terms the aki-richards
-equation takes on a matrix form
+Defining a gradient operator matrix
+ G matrix
+and lumping the constant terms the aki-richards
+equation can be written as a matrix product.
+
+(aki richards equation)
+
+The seismic reflection data can now be modelled as a convolution of a wavelet with a
+reflectivity series. Convolution is a linear operation, so by definition it
+can be expressed in matrix form. This operator is a special type of matrix,
+called a Toeplitz matrix, where each row is a shifted copy of the previous row.
+
+Combining the Aki-Richards operator with the convolution operator we
+get the forward modelling operator A. Our angle gather can be synthesized by the
+matrix vector product Am=d. 
 =#
 
-#=
-Seismic reflection data can be modelled as a convolution of a wavelet with a
-reflectivity series. Convolution is a linear operation, so by definition it
-can be expressed in matrix form. A convolution operator has a special form
-called a Toeplitz matrix, which I form with a 10 Hz Ricker wavelet.
-=#
-dt, f = .001, 10.0
-duration = dt * n * 2
+# helper functions available on github
+include("helper.jl")
     
+# Build a 1D 2-layer earth model
+n = 250
+vp, vs, rho = zeros(n+1), zeros(n+1), zeros(n+1)
+vp[1:n/2] = 2750; vs[1:n/2] = 1600; rho[1:n/2] = 2150.0;
+vp[n/2:end] = 3000; vs[n/2:end] = 2200; rho[n/2:end] = 2500.0;
+
+
+m = [rho, vp, vs]
+
+# Make the Aki Richards reflectivity operator for 11 angles [0-20 deg]
+theta = [0:2:40]
+R = [opAkiRichards(n, angle, mean(vp), mean(vs), mean(rho))
+     for angle in theta]
+R = cat(1, R...)
+
+# Make a 40 Hz Ricker wavelet
+dt, f = .001, 40.0
+duration = dt * (n-1) * 2  
 w = Ricker(duration, dt, f)
-C = full(Toeplitz(w))
+
+# Make a single Toeplitz convolution matrix
+C0 = opToeplitz(w)
+
+# Expand the convolution matrix to match the offset dimensions
+# using a kronecker matrix product. 
+C =  kron(speye(size(theta,1)), C0)
+
+# Make the forward modelling operator A
+A = C*R
+
+# forward model the data
+d = A*m
+
+# reshape the data vector into the proper dimensions
+d = reshape(d, n, size(theta,1))
+
+# Plot the gather
+plot(d)
+
 
 
 
--- helper.jl
+++ helper.jl
@@ -1,3 +1,7 @@
+@pyimport matplotlib.pyplot as plt
+import SpecialMatrices
+import PyCall
+
 function Ricker(duration, dt, f)
 
     freq = f
@@ -10,6 +14,58 @@ function Ricker(duration, dt, f)
     A = (1 - (2 * pft)) .* exp(-pft)
 
     return A
+end
+
+
+function plot(data)
+    
+    plt.figure()
+    plt.imshow(data, aspect="auto", cmap="Greys")
+    
+    plt.savefig("imshow.eps")
+    
+    plt.figure()
+    for i in 1:size(data,2)
+        plt.plot(data[:,i] + .3*(i-1), 1:size(data,1), "b") 
+        plt.fill_betweenx(1:size(data,1), ones(size(data,1)) * .3*(i-1),
+                          data[:,i] + .3*(i-1),
+                          data[:,i] .> 0, edgecolor="b")
+    end
+
+    plt.title("Synthetic Angle Gather")
+    plt.xlabel("Angle")
+    plt.ylabel("Time")
+
+    plt.xlim([-.3, size(data,2)*.3 + .3])
+    plt.xticks([0,size(data,2)-1]*.3, [0,40])
+    plt.yticks([])
+    plt.tight_layout()
+    plt.savefig("wiggles.eps")
+end
+
+function opToeplitz(vector)
+
+    return sparse(full(Toeplitz(vector)))
+end
+
+function opGrad(n)
+    # Returns a difference operator
+    D = spdiagm(tuple(ones(n) * -1, ones(n) *1),[0,-1])
+    return D'
+end
+
+function opAkiRichards(n, theta, vp_av, vs_av, rho_av)
+    
+    G = opGrad(n)
+    
+    theta = deg2rad(theta)
+
+    A = (1/rho_av)*(1/2.)*(1- (4*((vs_av / vp_av)^2)) * sin(theta)^2)*G
+    B = (1/vp_av)*(1/2)*(1+ (tan(theta)^2))*G
+    C = ((1/vs_av) * 4 *((vs_av/vp_av)^2) * sin(theta)^2)*G
+
+    return [A B C]
+
 end
 
 function opNMO(n, fold, velocity, dt, dx)
